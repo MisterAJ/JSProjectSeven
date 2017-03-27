4da3ac95f90791950bc65e2a2901473f2a01baf4
AJ Longstreet <ajlongstreet@gmail.com>
2017-03-26 22:55:01 -0400

    Completed Basic Project

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     const user = twit.getUser(login);
+    req.directMessages = twit.getDirectMessages(user);
     req.timelineTweets = twit.getTimeline(user);
+    req.followingUsers = twit.getFollowing(user);
     next()
 });
 
@@ -18,15 +20,33 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function (req, res) {
-
+    let timeline;
+    let following;
+    let messages;
     req.timelineTweets.then(function(info) {
-        res.render(__dirname + '/templates/index.pug',{timeline: info.data});
-        console.log(info.data);
+        timeline = info.data;
+        req.directMessages.then(function (info) {
+            messages = info.data;
+            req.followingUsers.then(function (info) {
+                following = info.data.users;
+                console.log(messages);
+                res.render(__dirname + '/templates/index.pug',{timeline: timeline,
+                                                                following: following,
+                                                                messages: messages});
+            })
+        })
     })
 });
 
 app.listen(80, function () {
     console.log('Frontend Server running on port 80')
 });
+
+
+
+
+
+
+
 
 
