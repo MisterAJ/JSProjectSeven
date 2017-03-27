309dbf9b3d5a41a82d84eb7576c6df5e53aca12d
AJ Longstreet <ajlongstreet@gmail.com>
2017-03-27 11:39:31 -0400

    Added configuration Template for OAuth

---
+++ src/configTemplate.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    // Put your info below and change the file name to config.js
+
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
+    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
+};
