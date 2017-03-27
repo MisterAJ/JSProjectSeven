3d40c0d7d934f203bdb6b69f513b5a720ab7b8df
AJ Longstreet <ajlongstreet@gmail.com>
2017-03-27 18:23:14 -0400

    Adding proper commenting

--- src/app.js
+++ src/app.js
@@ -1,35 +1,48 @@
 'use strict';
 
+// Please connect via localhost:8080
+
+// Set up Express, Twit and Twit's Config file
 const express = require('express');
 const login = require(__dirname + '/config.js');
 const twit = require(__dirname + '/twit.js');
 
 const app = express();
 
+// Assign directory for static resources
 app.use(express.static(__dirname + '/public'));
 
+// Middleware to pull Twitter objects and assign to request keys
 app.use((req, res, next) => {
+    // Pulls User object from twitter for use in later requests
     const user = twit.getUser(login);
+
+    // Users user object to request Twitter objects and assign to the request
     req.directMessages = twit.getDirectMessages(user);
     req.timelineTweets = twit.getTimeline(user);
     req.followingUsers = twit.getFollowing(user);
     next()
 });
 
+// Set pug to control the view engine
 app.set('view engine', 'pug');
+// Set the template directory
 app.set('views', __dirname + '/templates');
 
+// Get handler for the main page
 app.get('/', function (req, res) {
     let timeline;
     let following;
     let messages;
+    // Nested promise objects to assure that the twitter objects have loaded before
+    // sending them along with the view
     req.timelineTweets.then(function(info) {
         timeline = info.data;
         req.directMessages.then(function (info) {
             messages = info.data;
             req.followingUsers.then(function (info) {
                 following = info.data.users;
-                console.log(messages);
+                // rendering page and passing twit objects with the response
                 res.render(__dirname + '/templates/index.pug',{timeline: timeline,
                                                                 following: following,
                                                                 messages: messages});
@@ -38,8 +51,9 @@ app.get('/', function (req, res) {
     })
 });
 
-app.listen(80, function () {
-    console.log('Frontend Server running on port 80')
+// Start web server to listen on port 8080
+app.listen(8080, function () {
+    console.log('Frontend Server running on port 8080')
 });
 
 
--- src/twit.js
+++ src/twit.js
@@ -1,21 +1,27 @@
+// Enables the Twit plugin to be used
 const Twit = require('twit');
 
+// This function takes a twitter OAuth object and pulls a User object
 function getUser(login) {
     return new Twit(login);
 }
 
+// This function takes a Twitter user object and pulls the latest 5 sent messages
 function getDirectMessages(user) {
     return user.get('direct_messages/sent', { screen_name: user.screen_name, count: 5 });
 }
 
+// This function takes a Twitter user object and pulls the 5 latest tweets from your timeline
 function getTimeline(user) {
     return user.get('statuses/user_timeline', { screen_name: user.screen_name, count: 5 });
 }
 
+// This function takes a Twitter user object and pulls 5 people you are following
 function getFollowing(user) {
     return user.get('friends/list', { screen_name: user.screen_name , count: 5 });
 }
 
+// Module exports to allow the functions to be used in the other JS files
 module.exports.getUser = getUser;
 module.exports.getDirectMessages = getDirectMessages;
 module.exports.getTimeline = getTimeline;
